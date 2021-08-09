e4903610398955fa63e9bd95203efdc718b2437a
James Arbogast <jamesarbogast52@gmail.com>
2021-08-09 13:12:19 -0700

    Dojodach init commit

---
+++ Models/Dojodachi.cs
@@ -0,0 +1,27 @@
+
+namespace Dojodachi.Models
+{
+  public class Dachi {
+    public int Happiness {get; set;}
+    public int Fullness {get; set;}
+    public int Energy {get; set;}
+    public int Meals {get; set;}
+
+    public int Feed()
+    {
+      return Fullness;
+    }
+    public int Play()
+    {
+      return Happiness;
+    }
+    public int Feed()
+    {
+      return Fullness;
+    }
+    public int Feed()
+    {
+      return Fullness;
+    }
+  }
+}
